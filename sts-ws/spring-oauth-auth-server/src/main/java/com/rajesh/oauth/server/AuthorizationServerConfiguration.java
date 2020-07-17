88aebb6cb8f6b0a5e9994c500ca9b0e0c944d979
Rajesh <rajeshkannang87@gmail.com>
2020-07-17 11:19:17 +0530

    Spring Boot OAUTH Authorization Server

---
+++ sts-ws/spring-oauth-auth-server/src/main/java/com/rajesh/oauth/server/AuthorizationServerConfiguration.java
@@ -0,0 +1,51 @@
+package com.rajesh.oauth.server;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter{
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception{
+		clients.inMemory()
+			.withClient("rajeshclientid")
+			.authorizedGrantTypes("password", "authorization_code")
+			.secret(encoder().encode("secret"))
+			.scopes("user_info")
+			.redirectUris("http://localhost:8443/myapp/login/oauth2/code/sampleapp")
+			.autoApprove(false)
+			
+			.and()
+			
+			.withClient("microclient")
+			.authorizedGrantTypes("password", "authorization_code","client_credentials")
+			.secret(encoder().encode("secret"))
+			.scopes("user_info")
+			.autoApprove(false)
+			;
+		
+	}
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+		endpoints.authenticationManager(authenticationManager);
+	}
+	
+	@Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ sts-ws/spring-oauth-auth-server/src/main/java/com/rajesh/oauth/server/SpringOauthAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.rajesh.oauth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauthAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauthAuthServerApplication.class, args);
+	}
+
+}
